21d702360405de618ced329009a3b215fc9b54db
Miranda White <DesignsofPurpose@gmail.com>
2021-05-03 17:44:31 -0700

    added in color change
    toggle img only works for 2 pics so far.

--- HMWRK/Week10/script.js
+++ HMWRK/Week10/script.js
@@ -1,5 +1,5 @@
 
-<script>
+
 function myFunction() {
       var x = document.getElementById("myDIV1");
       var y = document.getElementById("myDIV2");
@@ -12,4 +12,40 @@ function myFunction() {
     
       }
     }
-</script>
+    let header = document.getElementById("myHeader");
+    let clrBTN = document.getElementById("colorBTN");
+    
+    let quokkaImg = document.getElementById("quokkaImg");
+    let tgglBTN = document.getElementById("toggleBTN");
+    
+    function changeTheColor(){
+        // WE'LL DO SOME COLOR CHANGING HERE
+        let redComp = Math.random() * 255;
+        let greenComp = Math.random() * 255;
+        let blueComp = Math.random() * 255;
+        console.log("RED: " + redComp);
+        console.log("GREEN: " + greenComp);
+        console.log("BLUE: " + blueComp);
+    
+        header.style.backgroundColor = "rgb("+ redComp + ", "+ greenComp +", " + blueComp + ")";
+    }
+    
+    let toggleTheImage = function(){
+        // WE'LL DO SOME IMAGE TOGGLING HERE
+    
+        if(tgglBTN.innerText === "Show the second one!") {
+            console.log("show the second image");
+            quokkaImg.src = "quokka2.jpg";
+            tgglBTN.innerText = "Now show the first one!";
+        }
+        else {
+            console.log("show the first image");
+            quokkaImg.src = "quokka1.webp";
+            tgglBTN.innerText = "Show the second one!";
+        }
+    }
+    
+    changeTheColor();
+    
+    clrBTN.addEventListener("click", changeTheColor);
+    tgglBTN.addEventListener("click", toggleTheImage);

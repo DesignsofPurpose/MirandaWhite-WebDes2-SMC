b3d05a5310b0fc93f37b4ecc7ec8847e47b67b7d
Miranda White <DesignsofPurpose@gmail.com>
2021-05-08 09:55:26 -0700

    working on CSS

--- HMWRK/Week10/script.js
+++ HMWRK/Week10/script.js
@@ -1,28 +1,11 @@
-
-
-
-function myFunction() {
-  var x = document.getElementById("myDIV1");
-  var y = document.getElementById("myDIV2");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.style.display = "none"
-  } else {
-    x.style.display = "none";
-    y.style.display = "block";
-
-
-  }
-}
 let header = document.getElementById("myHeader");
-let div = document.getElementById("myDIV1")
+let displayIMG = document.getElementById("displayIMG")
 let clrBTN = document.getElementById("colorBTN");
-
-let quokkaImg = document.getElementById("quokkaImg");
 let tgglBTN = document.getElementById("toggleBTN");
 
-function changeTheColor(){
-    // WE'LL DO SOME COLOR CHANGING HERE
+
+  function changeTheColor(){
+    // COLOR CHANGING METHOD //
     let redComp = Math.random() * 255;
     let greenComp = Math.random() * 255;
     let blueComp = Math.random() * 255;
@@ -31,23 +14,43 @@ function changeTheColor(){
     console.log("BLUE: " + blueComp);
 
     header.style.backgroundColor = "rgb("+ redComp + ", "+ greenComp +", " + blueComp + ")";
-}
-
-let toggleTheImage = function(){
-  // WE'LL DO SOME IMAGE TOGGLING HERE
-
-  if(tgglBTN.innerText === "Show the second one!") {
-      console.log("show the second image");
-      sisters.src = "quokka2.jpg";
-      tgglBTN.innerText = "Now show the first one!";
   }
-  else {
-      console.log("show the first image");
-      sisters.src = "pexels-lisa-2846828.jpg";
-      tgglBTN.innerText = "Show the second one!";
+
+// TOGGLE IMAGE METHOD //
+let toggleTheImage = function(){
+
+  if (toggleBTN.innerText === "Show the second one!") {
+      console.log("Show the second image.");
+      displayIMG.src = "pexels-lisa-3616590.jpg";
+      toggleBTN.innerText = "Now show the third one!";return;
+  }
+  if(toggleBTN.innerText === "Show the third image.") {
+    console.log("Show the third image.");
+    displayIMG.src = "pexels-matheus-bertelli-4085685.jpg";
+    toggleBTN.innerText = "Now show the fourth one!";return;
+  }
+  if(toggleBTN.innerText === "Show the fourth one!") {
+  console.log("Show the fourth image");
+  displayIMG.src = "pexels-lisa-2846828.jpg";
+  toggleBTN.innerText = "Now show the first one!";return;
+  }
+  if(toggleBTN.innerText === "Show the first one!") {
+    console.log("Show the first image");
+    displayIMG.src = "pexels-lisa-3586766.jpg";
+    toggleBTN.innerText = "Now show the second one!";return;
+    }
+
+
+
+
+  // else {
+      // console.log("show the first image");
+      // sisters.src = "pexels-lisa-2846828.jpg";
+      // tgglBTN.innerText = "Show the second one!";
   }
 }
 
 changeTheColor();
 
 clrBTN.addEventListener("click", changeTheColor);
+tgglBTN.addEventListener("click", toggleTheImage);

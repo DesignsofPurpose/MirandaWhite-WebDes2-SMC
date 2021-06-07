b24c35166de9a439935fc022db1bbd0bea47b940
Miranda White <DesignsofPurpose@gmail.com>
2021-06-07 15:15:55 -0700

    2 out of 3 task
    next: responsive

---
+++ HMWRK/Final/script.js
@@ -0,0 +1,49 @@
+// $(document).ready(function(){
+//     $(".logo").click(function(){
+//         $(".navlinks").slideToggle();
+//     });
+
+    // $(".aa").click(function(){
+    //     $(".a-p").toggle();
+    // });
+        
+    // $(".bb").click(function(){
+    //     $(".b-p").toggle();
+    // });
+        
+    // $(".cc").click(function(){
+    //     $(".c-p").toggle();
+    // });
+
+    // $(".dd").click(function(){
+    //     $(".d-p").toggle();
+    // }); 
+
+    // $(".ee").click(function(){
+    //     $(".e-p").toggle();
+    // });
+
+    // $(".ff").click(function(){
+    //     $(".f-p").toggle();
+    // });
+
+    // $(".a1").click(function(){
+    //     $(".ad1-p").toggle();
+    // });
+
+    // $(".a2").click(function(){
+    //     $(".ad2-p").toggle();
+    // });
+
+    // $(".a3").click(function(){
+    //     $(".ad3-p").toggle();
+    // });
+
+    // $(".a4").click(function(){
+    //     $(".ad4-p").toggle();
+    // });
+
+//     $(".donatebutton").on('click', function () {
+//         alert("You're PAWSOME!");
+//     });
+// });
